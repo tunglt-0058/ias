31d96f7379a720a2394706d275e3da90e8dc43f4
Le Tat Tung <letattungtb@gmail.com>
2021-06-29 15:02:10 +0900

    fix stock detail page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,17 @@
 class StocksController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :find_stock, only: :show
 
   def show
+    @same_industry_stocks = Stock.where(sector_id: @stock.sector_id, industry_id: @stock.industry_id)
+  end
+
+  private
+  def find_stock
+    @stock = Stock.find_by_display_id(params[:display_id].upcase)
+    unless @stock
+      flash[:danger] = t "not_found.stock"
+      redirect_to not_found_index_path
+    end
   end
 end
